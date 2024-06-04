70663b0144ac3ba6d4da5166caba2370629d6707
guts <guts20002120>
2024-06-04 10:57:22 +0330

    giignore and databae

--- test.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from PIL import Image, ImageTk
-
-root = tk.Tk()
-ico = Image.open('test.jpg')
-ico.thumbnail((25,25))  # Replace with your image file
-photo = ImageTk.PhotoImage(ico)
-root.wm_iconphoto(False, photo)
-root.mainloop()
